9ba65bf06962be55ee7b8fe487b09e1c68236608
hackerhunt <mikeyseerapu@gmail.com>
2025-04-23 07:12:01 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,232 @@
+name = input('enter your name:')
+print("hello",name,"welcome to our house")
+
+
+
+
+
+
+
+arr = [5,2,4,9]
+
+print(arr[0])
+length_list = len(arr)
+
+req__index = arr.index(4)
+
+print(req__index)
+
+print(2 in arr)
+
+
+#print(max(arr))
+#print(min(arr))
+
+#print('before soting : ',end="")
+#print(arr)
+#arr.sort()
+#print('after sorting : ',end="")
+#print(arr)
+
+#arr.remove(9)
+#print(arr)
+
+   #Tuple in python
+#    0 1 2 3 4
+t = (4,2,5,1,3)
+l = [4,2,5,1,3]
+
+#print(type(t))
+#print(type(l))
+
+#l[0] = 102
+#print(l)
+
+#t(0) = 23
+#print(t)
+
+#print('before sorting : ',end="")
+#print(l)
+#l.sort()
+#print('after sorting : ',end="")
+#print(l)
+
+             # set in python
+             # no duplicate values
+
+l = [3,3,3,2,2,1,1]
+
+s = {3,3,3,2,2,1,1}
+
+#print('list data: ',l)
+#print('set data : ',s)
+
+#s.add(8)
+#s.add(9)
+
+#print(s)
+
+            # important functions set
+
+# max
+# min
+# len
+# remove
+# add
+
+#print(len(s))
+#print(max(s))
+#print(min(s))
+
+            # dictionies in python , key:value
+
+
+d = {}
+
+            # two ways of inserting data into the dic
+#d['name']= 'ramana'
+#d.__setitem__('country','india')
+
+#print(d)
+
+            # how to acess the valuse in dic
+#print(d['name'])
+#print(d.get('name'))
+
+
+        # funcationalities of dic
+
+dictionary = {1:10,2:20,3:30,4:40}
+
+#print(len(dictionary))
+#print(max(dictionary))
+#print(min(dictionary))
+            # add
+
+#dictionary[key] = value
+#dictionary.__setitem__(value)
+
+            #accessing the data
+
+#dictionary[key] => None
+#dictionary.get(key)
+
+                #indentation
+
+#if False:
+   # print('hello world')
+   # if True:
+      #  print('another block')
+
+#else:
+   # print('not hello world!')
+    
+
+              # Operators in python
+
+    #+ => addtion
+    #- => sub
+    #* => mul
+    #/ =>
+    #% => reminder
+    #2**3 => 8, ** power calculations
+    #//
+    #5/2 => 2.5
+    #5//2 => 2
+
+#a = 4
+#b = 3
+#add = a+b
+#sub = a-b
+#mul = a*b
+#div = a/b
+#p = a**b
+#integer_div = a//b
+#print('addtion : ',add)
+#print('sub : ',sub)
+#print('mul : ',mul)
+#print('division : ',div)
+#print('power : ',p)
+#print('interger div : ',integer_div)
+
+#==
+#<
+#>
+#<=
+#>=
+#!=
+
+#a = 10
+#b = 11
+
+#print(a == b)
+#print(a<b)
+#print(a>b)
+#print(a<=b)
+#print(a>=b)
+#print(a!=b)
+
+
+                #logical operators
+                # and
+                # or
+
+#c = True
+#print(not c)
+
+# in
+# not in
+
+l =[1,2,3,5,7,9,10]
+# print(5 not in l)
+
+# is
+# is not
+a = 55
+b = 66
+# print(a is b)
+# print(a is not b)
+
+        # bitwsie operators
+
+# &
+# |
+# ^
+        # If Eles staments in python language
+
+# age = 18
+
+# if age>=14:
+   # print("True")
+# else:
+   # print("False")
+    
+# if age>=20:
+   # print("True")
+# else:
+   # print("False")
+
+# name = input('enter your name :')
+# age = int(input('enter your name :'))
+
+# if age<18:
+   #  print('sorry',name,'your are not eligible for this job')
+ # else:
+ #   print("hello",name,"welcome to the page")
+ 
+# marks = float(input("enter your marks"))
+
+# if marks >=90:
+    # print('your grade is : A')
+# elif marks >=80 and marks <90:
+    # print('your grade is B')
+# elif marks >=70 and marks <80:
+   #  print('your grade is C')
+#else:
+   # print("sorry your are failed!")
+
+
+
+
+
+    
